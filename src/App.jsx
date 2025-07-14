65ce93732ce89ad48a9928c326273347fab38b37
Panashi <48622695+bonanza-olaf@users.noreply.github.com>
2025-07-14 14:12:10 +0000

    Update siteProps with user information and add animation to social icons

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Panashi",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "panashi@example.com",
+  gitHub: "bonanza-olaf",
+  instagram: "example",
+  linkedIn: "example",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "panashi785917",
+  youTube: "example",
 };
 
 const primaryColor = "#4E567E";
